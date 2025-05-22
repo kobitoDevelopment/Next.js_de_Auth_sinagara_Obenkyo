5099f709fa0439009a3b45bd1d1c8e10aaab69c3
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-05-22 15:11:48 +0900

    Delete src directory

--- src/app/components/common/footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="copyright">copyright</div>
-    </footer>
-  );
-}
--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// URLとキーは環境変数から読み込むのがベスト（.env.local に定義）
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
