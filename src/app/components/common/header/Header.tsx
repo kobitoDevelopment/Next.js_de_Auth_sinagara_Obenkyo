440fbadd119e59cc7e7d33421e399a1d5420285c
I_AM_NOT_SPAM <95177138+kobitoDevelopment@users.noreply.github.com>
2025-05-22 22:29:31 +0900

    Add files via upload

--- src/app/components/common/header/Header.tsx
+++ src/app/components/common/header/Header.tsx
@@ -1,7 +1,9 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <header className="header">
-      <h1>header</h1>
+    <header className={styles.header}>
+      <h1>to do app</h1>
     </header>
   );
 }
