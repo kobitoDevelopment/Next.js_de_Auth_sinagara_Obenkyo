26d1ed72a2b59ce8b0ab46e4f43728683ab0e4ac
u_tkng <kobito@kobitonoMacBook-Air.local>
2025-05-30 01:52:31 +0900

    スタイル修正

--- src/app/components/common/header/Header.tsx
+++ src/app/components/common/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>to do app</h1>
+      <h1 className={styles.title}>to do app</h1>
     </header>
   );
 }
