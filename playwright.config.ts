cdcdb755a5d1e3721f1ea3aff9adcb7ef8ff6cbd
u_tkng <kobito@kobitonoMacBook-Air.local>
2025-06-17 22:03:32 +0900

    E2Eテストはローカル環境でのみ実行するよう変更

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* ベースURLの設定 */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -79,7 +79,7 @@ export default defineConfig({
   // ウェブサーバーの自動起動設定
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 60 * 1000, // 1分のタイムアウト
   },
